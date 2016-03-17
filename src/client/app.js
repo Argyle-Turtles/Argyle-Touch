ed1b9e3d20c12041920603ddb2c613bf819a1c0f
Peter Gyory <ptg3662@rit.edu>
2016-03-17 10:36:42 -0400

    Hard code touch screen

--- src/client/app.js
+++ src/client/app.js
@@ -1,15 +1,29 @@
 // it begins here
 import Leap from 'leapjs';
-import { find, propEq } from 'ramda';
+import { find, propEq, allPass } from 'ramda';
 
 const getIndexFinger = find(propEq('type', 1));
+
+const halfHeight = 139.7;
+const halfWidth = 190.5;
+
+const breakPlane = finger => (finger.extended && finger.dipPosition[2] < 0.5);
+const xBound = x => (Math.abs(x) < halfWidth);
+const yBound = y => (y < halfHeight * 2);
+
+const inBounds = ({ dipPosition }) => (xBound(dipPosition[0]) && yBound(dipPosition[1]));
+
+const isTouch = finger => {
+  console.log(finger.dipPosition);
+  return allPass([breakPlane, inBounds])(finger);
+}
 
 export const init = () => {
   try {
     Leap.loop({
-
       hand: ({ fingers }) => {
-        if (getIndexFinger(fingers).extended) console.log('yes');
+        // console.log('hi');
+        console.log(isTouch(getIndexFinger(fingers)));
       },
     })
   }
