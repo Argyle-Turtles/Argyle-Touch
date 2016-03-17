7ddbf48307b0a34049c0602b0d37ced152cfad2c
Peter Gyory <ptg3662@rit.edu>
2016-03-17 09:35:54 -0400

    Check if index finger is extended

--- src/client/app.js
+++ src/client/app.js
@@ -1,13 +1,15 @@
 // it begins here
 import Leap from 'leapjs';
+import { find, propEq } from 'ramda';
+
+const getIndexFinger = find(propEq('type', 1));
 
 export const init = () => {
   try {
     Leap.loop({
 
-      hand: hand => {
-        console.log(hand);
-        debugger;
+      hand: ({ fingers }) => {
+        if (getIndexFinger(fingers).extended) console.log('yes');
       },
     })
   }
